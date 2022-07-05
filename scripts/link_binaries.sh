a6d9c981292259c61da2bbfccf8e2f600e22e7ee
Leandro Hermida <softdev@leandrohermida.com>
2022-07-05 12:09:12 -0400

    Update paths to external

--- scripts/link_binaries.sh
+++ scripts/link_binaries.sh
@@ -3,6 +3,6 @@
 cd $(git rev-parse --show-toplevel)
 mkdir bin
 cd bin
-ln -s ../pufferfish/build/src/pufferfish
-ln -s ../pufferfish/build/src/cedar
+ln -s ../external/pufferfish/build/src/pufferfish
+ln -s ../external/pufferfish/build/src/cedar
 cd ..
