3b43c02bf2abd6a2e10bc213e94dbe835e1d8b70
Leandro Hermida <softdev@leandrohermida.com>
2022-07-12 16:45:08 -0400

    Rename script; add auto dir tree creation if not exists

--- scripts/get_url_file.py
+++ scripts/get_url_file.py
@@ -1,4 +1,6 @@
 from argparse import ArgumentParser
+from os import makedirs
+from os.path import dirname
 from urllib.request import urlcleanup, urlretrieve
 
 if __name__ == "__main__":
@@ -6,6 +8,9 @@ if __name__ == "__main__":
     parser.add_argument("--file-url", type=str, help="File URL")
     parser.add_argument("--out-file", type=str, help="Output file")
     args = parser.parse_args()
+
+    out_dir = dirname(args.out_file)
+    makedirs(out_dir, mode=0o755, exist_ok=True)
 
     urlretrieve(args.file_url, filename=args.out_file)
     urlcleanup()
