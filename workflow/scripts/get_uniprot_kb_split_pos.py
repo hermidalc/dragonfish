506c111e7d322f8bb0593d3d21a19881e5d626c4
Leandro Hermida <softdev@leandrohermida.com>
2022-10-05 22:13:08 -0400

    Make logic even more efficient

--- workflow/scripts/get_uniprot_kb_split_pos.py
+++ workflow/scripts/get_uniprot_kb_split_pos.py
@@ -4,14 +4,18 @@ split_offsets = list(
     range(0, int(snakemake.params.kb_size), int(float(snakemake.params.split_size)))
 )
 
-entry_offset = 0
 split_pos = []
+entry_offset = 0
+split_offset = split_offsets.pop(0)
 with gzip.open(snakemake.input[0], "rb") as kb_fh:
     while line := kb_fh.readline():
-        if split_offsets and line.decode("utf-8").startswith("<entry "):
-            if entry_offset == split_offsets[0]:
+        if line.decode("utf-8").startswith("<entry "):
+            if entry_offset == split_offset:
                 split_pos.append(kb_fh.tell() - len(line))
-                split_offsets.pop(0)
+                if split_offsets:
+                    split_offset = split_offsets.pop(0)
+                else:
+                    break
             entry_offset += 1
 
 with open(snakemake.output[0], "wt") as out_fh:
