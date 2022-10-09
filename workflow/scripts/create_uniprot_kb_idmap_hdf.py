9f91fdbf358f8b88316dbdf8d70c0f5c2fdc6ddd
Leandro Hermida <softdev@leandrohermida.com>
2022-10-09 14:07:44 -0400

    Call id cds_id since EMBL/Genbank/DDBJ joint id

--- workflow/scripts/create_uniprot_kb_idmap_hdf.py
+++ workflow/scripts/create_uniprot_kb_idmap_hdf.py
@@ -12,14 +12,14 @@ split_num = 1
 num_split_entries = 0
 num_total_entries = 0
 split_uniprot_ids = []
-split_genbank_ids = []
+split_cds_ids = []
 with gzip.open(snakemake.input[0], "rt") as fh:
     for line in fh:
         uniprot_id, db_name, db_id = line.strip().split("\t")
         if db_name != "EMBL-CDS" or db_id == "-":
             continue
         split_uniprot_ids.append(uniprot_id)
-        split_genbank_ids.append(db_id)
+        split_cds_ids.append(db_id)
         num_split_entries += 1
         num_total_entries += 1
         if num_split_entries == int(float(snakemake.params.split_size)):
@@ -28,15 +28,15 @@ with gzip.open(snakemake.input[0], "rt") as fh:
             )
             print(f"Writing {hdf5_file}")
             vx.from_dict(
-                {"genbank_cds_id": split_genbank_ids, "uniprot_id": split_uniprot_ids}
+                {"cds_id": split_cds_ids, "uniprot_id": split_uniprot_ids}
             ).export_hdf5(hdf5_file)
             split_num += 1
             num_split_entries = 0
             split_uniprot_ids = []
-            split_genbank_ids = []
+            split_cds_ids = []
 hdf5_file = join(snakemake.output[0], f"{file_basename}_{split_num:02}.hdf5")
 print(f"Writing {hdf5_file}")
-vx.from_dict(
-    {"genbank_cds_id": split_genbank_ids, "uniprot_id": split_uniprot_ids}
-).export_hdf5(hdf5_file)
+vx.from_dict({"cds_id": split_cds_ids, "uniprot_id": split_uniprot_ids}).export_hdf5(
+    hdf5_file
+)
 print(f"Parsed {num_total_entries} entries")
