d3317582cd1020277e063e48f80707e8fba21acd
Leandro Hermida <softdev@leandrohermida.com>
2022-10-14 05:05:19 -0400

    Fix broken cmd and piping logic

--- wrappers/bio/seqkit/wrapper.py
+++ wrappers/bio/seqkit/wrapper.py
@@ -35,9 +35,12 @@ if cmd == "grep" and isinstance(pattern, (list, tuple)):
             flags.append([e])
 
     cmd_prefix = f"seqkit {cmd} --threads {snakemake.threads}"
-    shell_cmd = f" {flags[0]} {infiles} |"
-    shell_cmd += " | ".join(f"{cmd_prefix} {f}" for f in flags[1 : len(flags)])
-    shell_cmd += f"{cmd_prefix} --out-file {snakemake.output[0]} {log}"
+    shell_cmd = f"{cmd_prefix} {flags[0]} {infiles}"
+    if len(flags) > 2:
+        shell_cmd += " | " + " | ".join(
+            f"{cmd_prefix} {f}" for f in flags[1 : len(flags) - 1]
+        )
+    shell_cmd += f" | {cmd_prefix} {flags[-1]} --out-file {snakemake.output[0]} {log}"
 else:
     flags = ""
     if pattern is not None:
