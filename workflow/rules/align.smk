e6c5358d36f4094afe2030c187eaac3f80983a73
Leandro Hermida <softdev@leandrohermida.com>
2022-10-16 13:04:41 -0400

    Add Pufferfish align code and config

---
+++ workflow/rules/align.smk
@@ -0,0 +1,15 @@
+rule align_pufferfish_pe:
+    input:
+        index=PUFFERFISH_INDEX_DIR,
+        fq1=TRIMMED_FASTQ1_FILE,
+        fq2=TRIMMED_FASTQ2_FILE,
+    params:
+        pufferfish=abspath(join(config["pufferfish"]["bin_dir"], "pufferfish")),
+        extra=config["pufferfish"]["align"]["extra"],
+    output:
+        PUFFERFISH_ALIGN_FILE,
+    log:
+        PUFFERFISH_ALIGN_LOG,
+    threads: PUFFERFISH_ALIGN_THREADS
+    wrapper:
+        PUFFERFISH_ALIGN_WRAPPER
---
+++ wrappers/bio/pufferfish/align/wrapper.py
@@ -0,0 +1,39 @@
+__author__ = "Leandro C. Hermida"
+__email__ = "hermidalc@pitt.edu"
+__license__ = "BSD 3-Clause"
+
+from tempfile import gettempdir, TemporaryDirectory
+
+from snakemake.shell import shell
+
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
+pufferfish = snakemake.params.get("pufferfish")
+assert pufferfish is not None, "params: pufferfish is a required parameter"
+
+index = snakemake.input.get("index")
+assert index is not None, "input: index is a required input parameter"
+
+fq1 = snakemake.input.get("fq") or snakemake.input.get("fq1")
+assert fq1 is not None, "input: fq/fq1 is a required input parameter"
+fq2 = snakemake.input.get("fq2")
+if fq2:
+    in_fqs = f"--mate1 {fq1} --mate2 {fq2}"
+else:
+    in_fqs = f"--read {fq1}"
+
+extra = snakemake.params.get("extra", "")
+
+tmp_base_dir = snakemake.resources.get("tmpdir", gettempdir())
+
+with TemporaryDirectory(dir=tmp_base_dir) as tmp_dir:
+    shell(
+        "{pufferfish} align"
+        " --threads {snakemake.threads}"
+        " --index {index}"
+        " {in_fqs}"
+        " --output {snakemake.output[0]}"
+        " --tmpdir {tmp_dir}"
+        " {extra}"
+        " {log}"
+    )
