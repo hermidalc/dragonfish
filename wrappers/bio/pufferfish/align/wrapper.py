1ff79a24069f10635ede22403fe29cab58eeec2e
Leandro Hermida <softdev@leandrohermida.com>
2022-10-24 07:21:34 -0400

    Add PuffAligner support for sam, sam.gz, and pam files; use pigz instead of --compressedOutput

--- wrappers/bio/pufferfish/align/wrapper.py
+++ wrappers/bio/pufferfish/align/wrapper.py
@@ -2,6 +2,8 @@ __author__ = "Leandro C. Hermida"
 __email__ = "hermidalc@pitt.edu"
 __license__ = "BSD 3-Clause"
 
+from os.path import splitext
+
 from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
@@ -19,12 +21,28 @@ in_fqs = f"--mate1 {fq1} --mate2 {fq2}" if fq2 else f"--read {fq1}"
 
 extra = snakemake.params.get("extra", "")
 
+assert snakemake.output[0].endswith(
+    ("sam", "sam.gz", "pam")
+), "output: file type required to be sam, sam.gz, or pam"
+
+run_pigz = False
+if snakemake.output[0].partition[-1] == "pam":
+    output = snakemake.output[0]
+    extra += " --pam"
+elif snakemake.output[0].partition[-1] == "sam.gz":
+    output = splitext(snakemake.output[0])[0]
+    run_pigz = True
+else:
+    output = snakemake.output[0]
+
 shell(
     "{pufferfish} align"
     " --threads {snakemake.threads}"
     " --index {index}"
     " {in_fqs}"
-    " --outdir {snakemake.output[0]}"
+    " --outdir {output}"
     " {extra}"
     " {log}"
 )
+if run_pigz:
+    shell("pigz -p {snakemake.threads} {output}")
