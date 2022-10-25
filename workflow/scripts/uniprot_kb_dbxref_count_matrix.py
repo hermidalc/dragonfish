ff278d357f1d1e29bd4e9c2f21695df8aacb91ab
Leandro Hermida <softdev@leandrohermida.com>
2022-10-25 19:54:31 -0400

    dbxref mapping count matrix code

---
+++ workflow/scripts/uniprot_kb_dbxref_count_matrix.py
@@ -0,0 +1,25 @@
+import os
+
+import vaex as vx
+
+os.environ["MKL_NUM_THREADS"] = str(snakemake.threads)
+os.environ["NUMBA_NUM_THREADS"] = str(snakemake.threads)
+os.environ["NUMEXPR_NUM_THREADS"] = str(snakemake.threads)
+os.environ["OPENBLAS_NUM_THREADS"] = str(snakemake.threads)
+
+vx.settings.main.thread_count = snakemake.threads
+vx.settings.main.thread_count_io = snakemake.threads
+
+idmap_dbxref_df = vx.open(snakemake.input.idmap)
+genbank_counts_df = vx.open(snakemake.input.counts)
+
+dbxref_counts_df = idmap_dbxref_df.join(
+    genbank_counts_df,
+    how="inner",
+    left_on="genbank_id",
+    right_on="genbank_id",
+    allow_duplication=True,
+)
+dbxref_counts_df.drop("genbank_id", inplace=True)
+
+dbxref_counts_df.export_csv_arrow(snakemake.output[0], chunk_size=int(1e7))
