aba8e168d4f3d0f25b4caa06afa55d227f7b7967
Leandro Hermida <softdev@leandrohermida.com>
2022-10-26 13:41:13 -0400

    Update chunk size

--- workflow/scripts/convert_csv_to_hdf.py
+++ workflow/scripts/convert_csv_to_hdf.py
@@ -6,7 +6,7 @@ import vaex as vx
 with TemporaryDirectory(dir=snakemake.resources.get("tmpdir", gettempdir())) as tmp_dir:
     file_basename = basename(snakemake.input[0]).partition(".")[0]
     for i, df in enumerate(
-        vx.read_csv(snakemake.input[0], chunk_size=snakemake.params.chunk_size), start=1
+        vx.read_csv(snakemake.input[0], chunk_size=int(1e7)), start=1
     ):
         df.export_hdf5(join(tmp_dir, f"{file_basename}_{i:03}.hdf5"))
     vx.open(join(tmp_dir, f"{file_basename}_*.hdf5")).export_hdf5(
