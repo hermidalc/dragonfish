7d822a69c7f62b80b9189bcb3b1b4af285450ed1
Leandro Hermida <softdev@leandrohermida.com>
2022-10-29 11:39:03 -0400

    Fix samtools wrapper code and qname filter input

--- wrappers/bio/samtools/view/wrapper.py
+++ wrappers/bio/samtools/view/wrapper.py
@@ -4,15 +4,16 @@ __license__ = "BSD 3-Clause"
 
 from snakemake.shell import shell
 
-log = snakemake.log_fmt_shell(stdout=True, stderr=True, append=True)
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 bam = snakemake.input.get("bam")
 bam = snakemake.input[0] if bam is None else bam
 
 extra = snakemake.params.get("extra", "")
-qname = snakemake.params.get("qname")
+
+qname = snakemake.input.get("qname")
 if qname is not None:
-    extra = "--qname-file {qname} {extra}"
+    extra = f"--qname-file {qname} {extra}"
 
 shell(
     "samtools view"
