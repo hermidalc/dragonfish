a7a1eb2d59a44cd2903c28473edbee3d483eb390
Leandro Hermida <softdev@leandrohermida.com>
2022-10-29 10:10:51 -0400

    Fix Cedar wrapper pigz logic

--- wrappers/bio/pufferfish/cedar/wrapper.py
+++ wrappers/bio/pufferfish/cedar/wrapper.py
@@ -2,7 +2,11 @@ __author__ = "Leandro C. Hermida"
 __email__ = "hermidalc@pitt.edu"
 __license__ = "BSD 3-Clause"
 
+from os.path import splitext
+
 from snakemake.shell import shell
+
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 cedar = snakemake.params.get("cedar")
 assert cedar is not None, "params: cedar path is a required parameter"
@@ -11,22 +15,24 @@ assert snakemake.input[0].endswith(
     ("sam", "sam.gz", "pam")
 ), "input: file type required to be sam, sam.gz, or pam"
 
-gzip = snakemake.output[0].endswith((".gz", ".GZ"))
+inflag = "--puffMapperOut" if snakemake.input[0].endswith(".pam") else "--sam"
 
-flag = "--puffMapperOut" if snakemake.input[0].endswith(".pam") else "--sam"
+run_pigz = False
+if snakemake.output[0].endswith((".gz", ".GZ")):
+    output = splitext(snakemake.output[0])[0]
+    run_pigz = True
+else:
+    output = snakemake.output[0]
 
 extra = snakemake.params.get("extra", "")
-
-pigz = f"| pigz -p {snakemake.threads} -c 1> {snakemake.output[0]}" if gzip else ""
-
-log = snakemake.log_fmt_shell(stdout=False if gzip else True, stderr=True)
 
 shell(
     "{cedar}"
     " --threads {snakemake.threads}"
-    " {flag} {snakemake.input[0]}"
-    " --output {snakemake.output[0]}"
+    " {inflag} {snakemake.input[0]}"
+    " --output {output}"
     " {extra}"
-    " {pigz}"
     " {log}"
 )
+if run_pigz:
+    shell("pigz -p {snakemake.threads} {output}")
