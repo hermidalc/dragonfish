02983cfa2f08dc504daaf44fec28da0af4704ea7
Leandro Hermida <softdev@leandrohermida.com>
2022-11-03 07:44:36 -0400

    Formatting

--- workflow/scripts/file_list_from_paths.py
+++ workflow/scripts/file_list_from_paths.py
@@ -9,10 +9,7 @@ if sort_by:
     file_parts_df = pd.DataFrame.from_records(file_parts, columns=sort_by)
     sort_ascending = snakemake.params.get("sort_ascending", True)
     file_parts_df.sort_values(
-        by=sort_by,
-        ascending=sort_ascending,
-        inplace=True,
-        ignore_index=True,
+        by=sort_by, ascending=sort_ascending, inplace=True, ignore_index=True
     )
     file_parts = file_parts_df.to_records(index=False).tolist()
     files = [join(*p) for p in file_parts]
