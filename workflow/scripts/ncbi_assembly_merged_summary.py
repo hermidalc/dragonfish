1073b077baee81d14b6b1de5db9c34d36a16d1fe
Leandro Hermida <softdev@leandrohermida.com>
2022-11-08 07:54:08 -0500

    No need for conditional

--- workflow/scripts/ncbi_assembly_merged_summary.py
+++ workflow/scripts/ncbi_assembly_merged_summary.py
@@ -15,13 +15,13 @@ def load_summary_df(summary_file):
     return summary_df
 
 
-if len(snakemake.input) > 1:
-    merged_df = pd.DataFrame()
-    for summary_file in snakemake.input:
-        merged_df = pd.concat(
-            [merged_df, load_summary_df(summary_file)], verify_integrity=True
-        )
-else:
-    merged_df = load_summary_df(snakemake.input[0])
+merged_df = pd.DataFrame()
+for summary_file in snakemake.input:
+    merged_df = pd.concat(
+        [merged_df, load_summary_df(summary_file)],
+        axis=0,
+        ignore_index=True,
+        verify_integrity=True,
+    )
 
 merged_df.to_csv(snakemake.output[0], sep="\t")
