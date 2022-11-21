c1cf747a023a83d8ff7db1968e977b0849bccc2f
Leandro Hermida <softdev@leandrohermida.com>
2022-11-21 16:25:53 -0500

    Updates/fixes to impl

--- workflow/scripts/uniprot_kb_dbxref_count_matrix.py
+++ workflow/scripts/uniprot_kb_dbxref_count_matrix.py
@@ -11,8 +11,6 @@ vx.settings.main.thread_count = snakemake.threads
 vx.settings.main.thread_count_io = snakemake.threads
 
 genbank_counts_df = vx.open(snakemake.input.counts)
-genbank_counts_df.rename(genbank_counts_df.column_names[0], "genbank_id")
-
 idmap_dbxref_df = vx.open(snakemake.input.idmap)
 
 dbxref_counts_df = idmap_dbxref_df.join(
@@ -22,7 +20,6 @@ dbxref_counts_df = idmap_dbxref_df.join(
     right_on="genbank_id",
     allow_duplication=True,
 )
-
 dbxref_counts_df.drop(["genbank_id", "uniprot_id"], inplace=True)
-dbxref_counts_df.groupby(by="db_id", agg="sum", sort=True)
-dbxref_counts_df.export_csv_arrow(snakemake.output[0], chunk_size=int(1e7))
+dbxref_counts_df = dbxref_counts_df.groupby(by="db_id", agg="sum", sort=True)
+dbxref_counts_df.export_csv(snakemake.output[0], sep="\t", chunk_size=int(1e7))
