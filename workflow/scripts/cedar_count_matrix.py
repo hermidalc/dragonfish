807353a77937eb5d118a8cd9d55407261d9d93b3
Leandro Hermida <softdev@leandrohermida.com>
2022-11-21 13:10:24 -0500

    Fix merging and col names

--- workflow/scripts/cedar_count_matrix.py
+++ workflow/scripts/cedar_count_matrix.py
@@ -1,3 +1,5 @@
+from os.path import basename, splitext
+
 import pandas as pd
 
 
@@ -6,18 +8,20 @@ def load_cedar_df(cedar_file):
         cedar_file,
         sep="\t",
         index_col=0,
-        usecols=[0, 3],
+        header=0,
+        usecols=[0, 2],
+        names=["tax_id", splitext(basename(cedar_file))[0]],
         engine="c",
         low_memory=False,
     )
-    cedar_df.index.name = "tax_id"
     return cedar_df
 
 
 merged_df = pd.DataFrame()
 for cedar_file in snakemake.input:
     merged_df = pd.concat(
-        [merged_df, load_cedar_df(cedar_file)], axis=0, verify_integrity=True
+        [merged_df, load_cedar_df(cedar_file)], axis=1, verify_integrity=True
     )
 
+merged_df.fillna(0, inplace=True)
 merged_df.to_csv(snakemake.output[0], sep="\t")
