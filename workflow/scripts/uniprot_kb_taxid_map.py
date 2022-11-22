d950483f3891a0ad5261b2f740524006c0d5bfcb
Leandro Hermida <softdev@leandrohermida.com>
2022-11-22 15:32:49 -0500

    Write column header

--- workflow/scripts/uniprot_kb_taxid_map.py
+++ workflow/scripts/uniprot_kb_taxid_map.py
@@ -1,9 +1,10 @@
 import gzip
 
 with open(snakemake.output[0], "wt") as out_fh:
+    out_fh.write(f"accession\taccession.version\ttaxid\tgi\n")
     with gzip.open(snakemake.input[0], "rt") as in_fh:
         for line in in_fh:
             uniprot_id, db_name, db_id = line.strip().split("\t")
             if db_name == "NCBI_TaxID" and db_id != "-":
-                # Write zero (0) for last gi column
+                # Write zero (0) for gi column
                 out_fh.write(f"{uniprot_id}\t{uniprot_id}\t{db_id}\t0\n")
