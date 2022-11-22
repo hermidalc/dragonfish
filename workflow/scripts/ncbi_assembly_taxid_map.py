744cbca43dfe34a19df6870adaf47df481ccb887
Leandro Hermida <softdev@leandrohermida.com>
2022-11-22 15:47:04 -0500

    Print error instead of throwing one since found example of missing taxid

--- workflow/scripts/ncbi_assembly_taxid_map.py
+++ workflow/scripts/ncbi_assembly_taxid_map.py
@@ -24,6 +24,6 @@ with open(snakemake.output[0], "wt") as out_fh:
                     ref_id = fields[1].strip()
                     out_fh.write(f"{ref_id}\t{tax_id}\n")
 
-assert (
-    summary_tax_ids == tax_ids
-), f"Not all assembly taxids were found in acc2taxid: {summary_tax_ids.difference(tax_ids)}"
+missing_taxids = list(sorted(summary_tax_ids.difference(tax_ids)))
+if missing_taxids:
+    print(f"Not all assembly taxids were found in acc2taxid: {missing_taxids}")
