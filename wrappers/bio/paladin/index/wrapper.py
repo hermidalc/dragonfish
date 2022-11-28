3aa7109830c5cc195cf29beef4f69b10dfd3c508
Leandro Hermida <softdev@leandrohermida.com>
2022-11-28 11:01:01 -0500

    Fix impl

--- wrappers/bio/paladin/index/wrapper.py
+++ wrappers/bio/paladin/index/wrapper.py
@@ -3,7 +3,7 @@ __email__ = "hermidalc@pitt.edu"
 __license__ = "BSD 3-Clause"
 
 from os import remove
-from os.path import join
+from os.path import basename, join
 from shutil import copy
 
 from snakemake.shell import shell
@@ -11,20 +11,22 @@ from snakemake.utils import makedirs
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-ref_type = snakemake.input.get("ref")
+ref_type = snakemake.params.get("ref_type")
 assert ref_type is not None, "input: ref_type is a required parameter"
 assert int(ref_type) in range(1, 5), "input: ref_type must be 1..4"
 
 extra = snakemake.params.get("extra", "")
-gff = snakemake.input.get("gff", "")
 
-assert gff and ref_type == 1, "Error: ref_type must = 1 when providing gff file"
+gff = snakemake.input.get("gff", "")
+if gff:
+    assert ref_type == 1, "Error: ref_type must = 1 when providing gff file"
 
 makedirs(snakemake.output[0])
 
-new_input = join(snakemake.output[0], snakemake.input[0].partition(".")[0])
-copy(snakemake.input[0], new_input)
+ref = join(snakemake.output[0], basename(snakemake.input.ref).partition(".")[0])
 
-shell("paladin index -r{ref_type} {extra} {new_input} {gff} {log}")
+copy(snakemake.input.ref, ref)
 
-remove(new_input)
+shell("paladin index -r{ref_type} {extra} {ref} {gff} {log}")
+
+remove(ref)
