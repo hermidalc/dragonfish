2c65f85f3b67daac6378a84b214aad23d4ca60a8
Leandro Hermida <softdev@leandrohermida.com>
2024-11-07 07:00:28 -0500

    Add lower()

--- workflow/scripts/merged_gff.py
+++ workflow/scripts/merged_gff.py
@@ -2,14 +2,14 @@ import gzip
 
 with (
     gzip.open(snakemake.output[0], "wt")
-    if snakemake.output[0].endswith(".gz")
+    if snakemake.output[0].lower().endswith(".gz")
     else open(snakemake.output[0], "wt")
 ) as out_fh:
     out_fh.write(snakemake.params.header)
     for gtf_file in snakemake.input:
         with (
             gzip.open(gtf_file, "rt")
-            if gtf_file.endswith(".gz")
+            if gtf_file.lower().endswith(".gz")
             else open(gtf_file, "rt")
         ) as in_fh:
             for line in in_fh:
