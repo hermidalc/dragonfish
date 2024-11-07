62d90192fe5bce204b723656128e5d514c9315ff
Leandro Hermida <softdev@leandrohermida.com>
2024-11-07 06:58:54 -0500

    Formatting

--- workflow/scripts/merged_gff.py
+++ workflow/scripts/merged_gff.py
@@ -1,12 +1,16 @@
 import gzip
 
-with gzip.open(snakemake.output[0], "wt") if snakemake.output[0].endswith(
-    ".gz"
-) else open(snakemake.output[0], "wt") as out_fh:
+with (
+    gzip.open(snakemake.output[0], "wt")
+    if snakemake.output[0].endswith(".gz")
+    else open(snakemake.output[0], "wt")
+) as out_fh:
     out_fh.write(snakemake.params.header)
     for gtf_file in snakemake.input:
-        with gzip.open(gtf_file, "rt") if gtf_file.endswith(".gz") else open(
-            gtf_file, "rt"
+        with (
+            gzip.open(gtf_file, "rt")
+            if gtf_file.endswith(".gz")
+            else open(gtf_file, "rt")
         ) as in_fh:
             for line in in_fh:
                 if not line.startswith("#"):
